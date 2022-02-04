b2adfd62033ed741c075922f6132127f5631685a
ezesilva95 <4381@holbertonschool.com>
2022-02-04 09:03:53 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+*main - entry
+*Return: 0
+*/
+int main(void)
+{
+	char lowC, e, q;
+
+	e = 'e';
+
+	q = 'q';
+
+	for (lowC = 'a'; lowC <= 'z'; lowC++)
+	{
+		if (lowC != e && lowC != q)
+		{
+			putchar(lowC);
+		}
+		putchar('\n');
+		return (0);
+	}
+}
