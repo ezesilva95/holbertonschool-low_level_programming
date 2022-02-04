fa11bdd5961005b0702bb76a2c65065879800c14
ezesilva95 <4381@holbertonschool.com>
2022-02-04 11:29:10 -0800

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ *main - entry
+ *Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n < 100; n++)
+	{
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
+
+	if (n != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
