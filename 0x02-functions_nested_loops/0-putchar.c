4ba1ddb0569a392c8e1f593bee5c86c4eaaa90ed
ezesilva95 <4381@holbertonschool.com>
2022-02-07 07:09:28 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	char *put = "_putchar";
+
+	while (*put)
+	{
+		_putchar(*put);
+		put++;
+	}
+	_putchar('\n');
+	return (0);
+}
