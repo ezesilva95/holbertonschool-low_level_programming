60a68d002722823d32bd824355fa90ff746398f2
ezesilva95 <4381@holbertonschool.com>
2022-02-07 07:34:56 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+* main - entry
+*Return: 0
+*/
+void print_alphabet(void)
+{
+	int alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
