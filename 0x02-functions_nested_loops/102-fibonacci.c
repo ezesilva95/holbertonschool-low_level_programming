f125a34322b2ee051fc5f8ed13f513b7cbcdaaa5
ezesilva95 <4381@holbertonschool.com>
2022-02-07 18:45:00 -0800

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,24 +5,24 @@
  */
 int main(void)
 {
-	int sum, a, b, c;
+	unsigned long sum, a, b, c;
 
 	a = 0;
 	b = 1;
 
-	for (sum = 0; sum <= 50; sum++)
+	for (sum = 0; sum < 50; sum++)
 	{
 		c = a + b;
 		a = b;
 		b = c;
-		printf("%d", c);
-	if (sum == 50)
+		printf("%lu", c);
+	if (sum == 49 )
 	{
 		putchar('\n');
 	}
 	else
 	{
-		putchar(',');
+		printf(", ");
 	}
 	}
 	return (0);
