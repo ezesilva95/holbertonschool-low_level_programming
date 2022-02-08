57ee4e353bf22727d9eca2a654728495550952e6
ezesilva95 <4381@holbertonschool.com>
2022-02-07 17:48:26 -0800

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ *main - entry
+ *Return: 0
+ */
+int main(void)
+{
+	int n, res;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			res += n;
+		}
+		printf("%d\n", res);
+		return (0);
+	}
+}
