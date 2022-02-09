ea0153a2fcb8ef220c45b09472680ba013b56539
ezesilva95 <4381@holbertonschool.com>
2022-02-09 11:51:29 -0800

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@ void print_triangle(int size)
 {
 
 	int a, b;
-	
+
 	if (size <= 0)
 	{
 		_putchar('\n');
@@ -20,7 +20,7 @@ void print_triangle(int size)
 				_putchar(' ');
 			else
 				_putchar('#');
-			
+
 		}
 		_putchar('\n');
 	}
