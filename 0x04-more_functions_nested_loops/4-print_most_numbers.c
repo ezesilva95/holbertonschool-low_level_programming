d2d2b78e3c90294237348eeed5abe1bbb6f881c6
ezesilva95 <4381@holbertonschool.com>
2022-02-09 05:43:18 -0800

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_most_numbers - print numbers with some exeption
+ */
+void print_most_numbers(void)
+{
+	char n;
+
+	n = '0';
+	for (n = '0'; n <= '9'; n++)
+	{
+		if (n != '2' && n != '0')
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
