28f9986e195ab4499c646956e5d50b070dcfa1dc
ezesilva95 <4381@holbertonschool.com>
2022-02-09 07:14:34 -0800

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *more_numbers - prints ten times the numbers 0 to 14
+ */
+void more_numbers(void)
+{
+	int n, count;
+
+	for (count = 0; count <= 9; count++)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			if (n > 10)
+			{
+				_putchar('1');
+			}
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
