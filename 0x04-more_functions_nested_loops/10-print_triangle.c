e87cb0ae7adf74907c07afe129e88284d1b92d1b
ezesilva95 <4381@holbertonschool.com>
2022-02-09 11:49:20 -0800

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,23 +7,20 @@ void print_triangle(int size)
 {
 
 	int a, b;
-
+	
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
-	for (a = 0; a <= size; a++)
+	for (a = 1; a <= size; a++)
 	{
-		for (b = 0; b <= size; b++)
+		for (b = 1; b <= size; b++)
 		{
 			if (b <= (size - a))
-			{
 				_putchar(' ');
-			}
 			else
-			{
 				_putchar('#');
-			}
+			
 		}
 		_putchar('\n');
 	}
