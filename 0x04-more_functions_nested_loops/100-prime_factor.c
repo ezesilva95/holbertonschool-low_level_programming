88f5611279016de345a55ddf3f66800df4d30f75
ezesilva95 <4381@holbertonschool.com>
2022-02-10 11:47:02 -0800

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ *main - entry
+ *Return: 0
+ */
+int main(void)
+{
+	long int n;
+	long int div;
+	long int maxFactor;
+
+	n = 612852475143;
+	div = 2;
+
+	while (n != 1)
+	{
+		if (n % div != 0)
+		{
+			div = div + 1;
+		}
+		else
+		{
+			maxFactor = n;
+			n = n / div;
+		}
+		if (n == 1)
+		{
+			printf("%ld is the largest prim factor", maxFactor);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
