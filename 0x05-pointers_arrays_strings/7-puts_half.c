d828c3084fea42962afa6725dda38b759f9326be
ezesilva95 <4381@holbertonschool.com>
2022-02-11 15:07:39 -0800

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *puts_half - print half script
+ *@str: string
+ */
+void puts_half(char *str)
+{
+	int a = 0, b = 0, n;
+
+	while (str[a++])
+		b++;
+
+	if ((b % 2) == 0)
+	{
+		n = b / 2;
+	}
+	else
+	{
+		n = ((b - 1) / 2);
+	}
+	for (a = n; a < b; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
