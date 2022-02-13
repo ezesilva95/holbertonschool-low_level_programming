1a790de7d03538e7c49a840f1c7a4c1855794d28
ezesilva95 <4381@holbertonschool.com>
2022-02-12 18:29:51 -0800

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *_atoi - converts a string to an integer
+ *@s: string
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	int i, sign;
+	double a;
+
+	i = 0;
+	a = 0;
+	sign = 1;
+
+	while (s[i])
+	{
+		if (s[i] == '-')
+		{
+			sign = sign * -1;
+		}
+		if (s[i] - '0' >= 0 && s[i] - '0' <= 9)
+		{
+			a = (a * 10) + s[i] - '0';
+		}
+		else if (a != 0)
+		{
+			break;
+		}
+	i++;
+	}
+	return (a * sign);
+}
