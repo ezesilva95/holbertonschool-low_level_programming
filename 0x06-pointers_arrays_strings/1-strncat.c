6e6ce8d355b3c6557ab5ec7651eb5bc43eb9375b
ezesilva95 <4381@holbertonschool.com>
2022-02-15 06:12:26 -0800

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_strncat - function that concatenates two strings
+ *@dest: string one
+ *@src: string two
+ *Return: dest
+ *@n: asda
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0, b = 0;
+
+	while (dest[a])
+	{
+		a++;
+	}
+	while (b < n && src[b])
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a + n + 1] = '\0';
+
+	return (dest);
+}
