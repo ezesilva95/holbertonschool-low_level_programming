b1837b8933102335e9c04441f4b7d5f87bdd8194
ezesilva95 <4381@holbertonschool.com>
2022-02-16 06:10:45 -0800

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,6 +11,11 @@ char *cap_string(char *s)
 
 	a = 0;
 
+	if (s[a] >= 'a' && s[a] <= 'z')
+	{
+		s[a] -= 32;
+	}
+
 	while (s[a])
 	{
 		if (s[a] == ' ' || s[a] == '\t' || s[a] == '\n' || s[a] == ','
@@ -21,10 +26,6 @@ char *cap_string(char *s)
 			if (s[a + 1] >= 'a' && s[a + 1] <= 'z')
 			{
 				s[a + 1] = s[a + 1] - 32;
-			}
-			if (s[a] >= 'a' && s[a] <= 'z')
-			{
-				s[a] = s[a] - 32;
 			}
 		}
 		a++;
