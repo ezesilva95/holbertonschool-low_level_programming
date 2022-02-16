a90027e638e68fbe67ea4ee69482f84c4b925650
ezesilva95 <4381@holbertonschool.com>
2022-02-16 05:52:32 -0800

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *leet - function that encodes a string into 1337
+ *@s: puntero
+ *Return: s
+ */
+char *leet(char *s)
+{
+	int a, b;
+	int lett[10] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+	int num[10] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+
+	a = 0;
+	b = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; b <= 10; b++)
+		{
+			if (s[a] == lett[b])
+			{
+				s[a] = num[b];
+
+			}
+		}
+	a++;
+	}
+	return (s);
+}
