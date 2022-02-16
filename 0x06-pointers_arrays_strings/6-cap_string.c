9a258a6bf9c208ba619a36244abafda668b1487a
ezesilva95 <4381@holbertonschool.com>
2022-02-16 05:10:51 -0800

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+*cap_string - capitalizes all words of a string
+*@s: asd
+*Return: s
+*/
+char *cap_string(char *s)
+{
+
+	int a;
+
+	a = 0;
+
+	while (s[a])
+	{
+		if (s[a] == ' ' || s[a] == '\t' || s[a] == '\n' || s[a] == ','
+		|| s[a] == ';' || s[a] == '.' || s[a] == '!' || s[a] == '?' ||
+		s[a] == '"' || s[a] == '(' || s[a] == ')' ||
+		s[a] == '{' || s[a] == '}')
+		{
+			if (s[a + 1] >= 'a' && s[a + 1] <= 'z')
+			{
+				s[a + 1] = s[a + 1] - 32;
+			}
+			if (s[a] >= 'a' && s[a] <= 'z')
+			{
+				s[a] = s[a] - 32;
+			}
+		}
+		a++;
+	}
+	return (s);
+}
