e555517396a034873e451662fb34242119725089
ezesilva95 <4381@holbertonschool.com>
2022-02-16 08:29:40 -0800

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *rot13 - function that encodes using rot13
+ *@s: string
+ *Return: s
+ */
+char *rot13(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+	{
+		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
+		{
+			if ((s[a] >= 'a' && s[a] <= 'z') || (s[a] :wq:>= 'A' && s[a] <= 'Z'))
+			{
+				s[a] -= 13;
+				break;
+			}
+			s[a] += 13;
+			break;
+		}
+		a++;
+	}
+	return (s);
+}
+
