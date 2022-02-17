7a410ef118b671a2d32ccd33e7b15b2b02df5018
ezesilva95 <4381@holbertonschool.com>
2022-02-17 04:55:34 -0800

    Task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*print_number - funtion that prints integer
+*@n: number
+*/
+void print_number(int n)
+{
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	else
+	{
+		n = n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+		_putchar(n % 10 + '0');
+}
