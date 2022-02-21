f0b91a3d74339f7b48b77173620ef36fcc3c0492
ezesilva95 <4381@holbertonschool.com>
2022-02-21 08:59:51 -0800

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,12 +1,27 @@
 #include "main.h"
 /**
  *_strspn - function that gets the length of a prefix substring
- *@s: string 
+ *@s: string
  *@accept: string
  *Return: number of bytes
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int a; 
+	unsigned int a = 0, b, i = 0;
 
-	while 
+	while (accept[a])
+	{
+		b = 0;
+
+		while (s[b] != ' ')
+		{
+			if (accept[a] == s[b])
+			{
+				i++;
+			}
+			b++;
+		}
+	a++;
+	}
+	return (i);
+}
