d0acbdfb8ee391f9704cd208d072de938d287e7f
ezesilva95 <4381@holbertonschool.com>
2022-03-02 17:02:43 -0800

    Task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-*
-*
-*
+*free_grid - function that frees a 2 dimensional grid previously created
+*@grid: ags
+*@height: asg
+*Return: asf
 */
 void free_grid(int **grid, int height)
 {
