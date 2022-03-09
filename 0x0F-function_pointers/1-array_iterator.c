3dc2a9829fa209a70c707b42ced81f3e7eceae44
ezesilva95 <4381@holbertonschool.com>
2022-03-09 07:23:32 -0800

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ *array_iterator - function that executes a function
+ *@array: array
+ *@size: size
+ *@action:ad
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
