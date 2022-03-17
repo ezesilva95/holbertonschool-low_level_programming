47d136643466a05e9e61d29b1a77228a54406217
ezesilva95 <4381@holbertonschool.com>
2022-03-17 08:17:31 -0700

    Task 0

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 /**
  *list_len - functions that returns number of elements in a linked list_t list
  *@h: pointer to list
@@ -7,3 +7,11 @@
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
