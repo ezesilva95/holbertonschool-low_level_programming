6b706f730735f42635e4746dd763cda4fe83c748
ezesilva95 <4381@holbertonschool.com>
2022-03-21 07:07:20 -0700

    Task 3

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ *add_nodeint_end - function that adds a new node at the end of a listint_t list.
+ *@head: pointer to first element of the list.
+ *@n: int to set in new node.
+ *Return: address of the new element, or NULL if it failed
+ **/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *aux = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+
+	if (aux)
+	{
+		while (aux->next)
+		{
+			aux = aux->next;
+		}
+		aux->next = new;
+	}
+	else
+	{
+		*head = new;
+	}
+
+	return (new);
+}
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ *free_listint - frees a listint_t list.
+ *@head: point to the list
+ **/
+
+void free_listint(listint_t *head)
+{
+	listint_t *next_nd;
+	listint_t *actual_node;
+
+	if (head)
+	{
+		actual_node = head;
+		next_nd = head->next;
+
+		while (next_nd)
+		{
+			free(actual_node);
+			actual_node = next_nd;
+			next_nd = next_nd->next;
+		}
+		free(actual_node);
+	}
+}
