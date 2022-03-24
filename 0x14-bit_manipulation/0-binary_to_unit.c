fccc6a7d00aba9555ae1a74da40da5f22724804e
ezesilva95 <4381@holbertonschool.com>
2022-03-24 11:25:15 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -2,7 +2,7 @@
 /**
  * binary_to_uint - func. converts a binary number to an unsigned int.
  * @b: pointing to a string of 0 and 1 chars.
- * Return: the converted number if succes, or 0 
+ * Return: the converted number if succes, or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
