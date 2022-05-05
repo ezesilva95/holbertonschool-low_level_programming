58a3cfb59d075c4b72d8ac3857b5c720f4ce3b63
ezesilva95 <4381@holbertonschool.com>
2022-05-05 16:55:44 -0700

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,19 +7,19 @@
  */
 dlistint_t *insert_node_idx(dlistint_t *aux_node, int n)
 {
-	dlistint_t *new_node;
+        dlistint_t *new_node;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-	new_node->n = n;
+        new_node = malloc(sizeof(dlistint_t));
+        if (!new_node)
+                return (NULL);
+        new_node->n = n;
 
-	new_node->next = aux_node;
-	new_node->prev = aux_node->prev;
-	aux_node->prev->next = new_node;
-	aux_node->prev = new_node;
+        new_node->next = aux_node;
+        new_node->prev = aux_node->prev;
+        aux_node->prev->next = new_node;
+        aux_node->prev = new_node;
 
-	return (new_node);
+        return (new_node);
 }
 
 /**
@@ -31,22 +31,22 @@ dlistint_t *insert_node_idx(dlistint_t *aux_node, int n)
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *aux_node;
+        dlistint_t *aux_node;
 
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	if (!h)
-		return (NULL);
+        if (idx == 0)
+                return (add_dnodeint(h, n));
+        if (!h)
+                return (NULL);
 
-	aux_node = *h;
-	while ((idx != 0) && (aux_node->next))
-	{
-		idx -= 1;
-		aux_node = aux_node->next;
-		if (idx == 0)
-			return (insert_node(aux_node, n));
-	}
-	if (idx == 1)
-		return (add_dnodeint_end(h, n));
-	return (NULL);
+        aux_node = *h;
+        while ((idx != 0) && (aux_node->next))
+        {
+                idx -= 1;
+                aux_node = aux_node->next;
+                if (idx == 0)
+                        return (insert_node_idx(aux_node, n));
+        }
+        if (idx == 1)
+                return (add_dnodeint_end(h, n));
+        return (NULL);
 }
