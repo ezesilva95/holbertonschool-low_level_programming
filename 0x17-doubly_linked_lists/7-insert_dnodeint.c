18b6bcad2b4612e7d002a6ba0d0f1d9fd11491a6
ezesilva95 <4381@holbertonschool.com>
2022-05-05 15:24:06 -0700

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,9 +8,8 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *aux_node = *h;
+	dlistint_t *aux_node;
 	dlistint_t *new_node;
-	unsigned int count = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -21,22 +20,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new_node->next = *h;
-		*h = new_node;
-		return (*h);
+		return (add_dnodeint(h, n));
 	}
 
-	while (aux_node != NULL)
+	aux_node = *h;
+	while ((idx != 0) && (aux_node->next))
 	{
-		if (count == idx - 1)
-		{
-			new_node->next = aux_node->next;
-			aux_node->next = new_node;
-			return (new_node);
-		}
+		idx -= 1;
 		aux_node = aux_node->next;
-		count++;
+		if (idx == 0)
+		{
+			new_node = malloc(sizeof(dlistint_t));
+			if (new_node == NULL)
+				return (NULL);
+			new_node->n = n;
+			new->next = tmp;
+			new->prev = tmp->prev;
+			tmp->prev->next = new;
+			tmp->prev = new;
+			return (new);
+		}
 	}
-	free(new_node);
+	if (idx == 1)
+		return (add_dnodeint_end(h, n));
 	return (NULL);
 }
